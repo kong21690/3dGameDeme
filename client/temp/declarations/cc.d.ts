7763d0cad7dc60f5e3b4f592991079642942f762
kong21690 <32952616+kong21690@users.noreply.github.com>
2020-09-14 10:21:41 +0800

    temp

--- client/temp/declarations/cc.d.ts
+++ client/temp/declarations/cc.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="C:\CocosDashboard_1.0.9\resources\.editors\Creator3D\1.1.2\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+/// <reference path="/Applications/CocosCreator/Creator3D/1.1.2/CocosCreator3D.app/Contents/Resources/resources/3d/engine/bin/.declarations/cc.d.ts"/>
